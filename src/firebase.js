72ad54b70e7a937e4f586468a1ab75ce646d2116
gerald1006 <202220920@urp.edu.pe>
2025-04-08 18:39:49 -0500

    asdknas

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgu1YxXnilh3T-d8FC4LNsZUjaJh2x3HE",
-  authDomain: "react-sieii-firebase.firebaseapp.com",
-  projectId: "react-sieii-firebase",
-  storageBucket: "react-sieii-firebase.firebasestorage.app",
-  messagingSenderId: "671599667116",
-  appId: "1:671599667116:web:713dceec89ab822f3d3c09"
 };
 
 const app = initializeApp(firebaseConfig);
